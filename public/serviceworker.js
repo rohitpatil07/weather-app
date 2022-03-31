02c37221f6b4fa498c6a9ba481a7bc0e83d6ef88
Rohit Patil <rohitpatil07122001@gmail.com>
2022-03-31 18:01:50 +0530

    Completed PWA application
    Signed-off-by: Rohit Patil <rohitpatil07122001@gmail.com>

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "version-1";
-const urlsToCache = [ 'index.html', 'offline.html' ];
+const urlsToCache = [ 'index.html', 'offline.html' , 'images/logo.png' , "images/logo512.png" ];
 
 const self = this;
 
